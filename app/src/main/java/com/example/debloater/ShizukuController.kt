01d0cf2eccde788bf911f30d41efea611ad2b61f
Binit <jaiswalbinit605@gmail.com>
2026-06-09 00:23:58 +0530

    Update ShizukuController.kt

--- app/src/main/java/com/example/debloater/ShizukuController.kt
+++ app/src/main/java/com/example/debloater/ShizukuController.kt
@@ -1,1 +1,13 @@
+package com.example.debloater
+
+import android.os.Bundle
+import android.widget.Toast
+import android.app.Activity
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Universal Debloater Initialized!", Toast.LENGTH_LONG).show()
+    }
+}
 
